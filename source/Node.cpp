6358ddd6fc73e042bf98461ca02c54606e396d64
zz <xzrunner@gmail.com>
2020-02-04 05:52:23 +0800

    rename

--- source/Node.cpp
+++ source/Node.cpp
@@ -131,8 +131,8 @@ void Node::PortBack2Front(std::vector<PinDesc>& dst,
         PinDesc d;
 
 		auto& s = src[i];
-        d.name = s.var.name;
         d.type = TerrAdapter::TypeBackToFront(s.var.type);
+        d.name = s.var.full_name;
 
         dst.push_back(d);
 	}
