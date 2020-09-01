79cc84eb71561637614478e855e1fc253e48a9f1
zz <xzrunner@gmail.com>
2020-09-01 08:43:21 +0800

    rename

--- include/terrainlab/node_def_gen.h
+++ include/terrainlab/node_def_gen.h
@@ -13,19 +13,6 @@
 #define PARM_FILEPATH terraingraph/device/##PARM_NODE_CLASS##.parm.h
 #endif
 
-#define Bool    bool
-#define Int     int
-#define Int2    sm::ivec2
-#define Int3    sm::ivec3
-#define Int4    sm::ivec4
-#define Float   float
-#define Float2  sm::vec2
-#define Float3  sm::vec3
-#define Float4  sm::vec4
-#define Matrix4 sm::mat4
-#define String  std::string
-#define Array   std::vector
-
 class PARM_NODE_CLASS : public Node
 {
 public:
@@ -46,19 +33,6 @@ public:
 
 	RTTR_ENABLE(Node)
 };
-
-#undef Bool
-#undef Int
-#undef Int2
-#undef Int3
-#undef Int4
-#undef Float
-#undef Float2
-#undef Float3
-#undef Float4
-#undef Matrix4
-#undef String
-#undef Array
 
 #undef PARM_NODE_NAME
 #undef PARM_NODE_CLASS
